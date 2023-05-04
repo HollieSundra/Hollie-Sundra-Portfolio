cc4d08f41ac30dde2084fdf41588316bb70baea0
James Sundra <pojo226@gmail.com>
2023-05-03 22:40:27 -0700

    contact

--- hollie-sundra-portfolio/src/components/ContactMe.js
+++ hollie-sundra-portfolio/src/components/ContactMe.js
@@ -1,19 +1,71 @@
+
+import React, { useState } from "react";
 import "./ContactMeStyles.css";
 
-import React from "react";
+
 
 const ContactMe = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+    });
+    
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch("/api/submit-form", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }; 
+
     return (
         <div className="form">
-            <form>
-                <label>Your Name</label>
-                <input type="text"></input>
-                <label>Email</label>
-                <input type="email"></input>
-                <label>Subject</label>
-                <input type="text"></input>
-                <label>Message</label>
-                <textarea rows="6" placeholder="Message here" />
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Your Name</label>
+                <input 
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                ></input>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}></input>
+                <label htmlFor="subject">Subject</label>
+                <input 
+                type="text"
+                id="subject"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}></input>
+                <label htmlFor="message">Message</label>
+                <textarea 
+                 id="message"
+                 name="message"
+                 rows="6" 
+                 placeholder="Message here"
+                 value={formData.message}
+                 onChange={handleChange} />
                 <button className="btn">Submit</button>
             </form>
         </div>
