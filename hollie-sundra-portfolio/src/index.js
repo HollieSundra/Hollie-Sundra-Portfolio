735364403c8398d7bd7b37551c62395d2479b0b6
James Sundra <pojo226@gmail.com>
2023-05-03 19:32:05 -0700

    hashrouter

--- hollie-sundra-portfolio/src/index.js
+++ hollie-sundra-portfolio/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>,
+    </React.StrictMode>
 );
 
