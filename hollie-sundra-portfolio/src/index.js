ee51b838c1f6c3b3c23b9896d417a10d9c1dc212
James Sundra <pojo226@gmail.com>
2023-05-03 21:04:41 -0700

    ?

--- hollie-sundra-portfolio/src/index.js
+++ hollie-sundra-portfolio/src/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
-ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("src")).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>,
-  </Router>, 
-    document.getElementById("root")
+    </HashRouter>,
+  </React.StrictMode>
 );
 
